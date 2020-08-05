a5fdbdf7668d868732aa8a883a500703220b015e
Raghav <Raghav@Raghav>
2020-08-05 17:28:49 +0530

    My Project Commit

---
+++ Actitime/src/test/java/com/actitime/timetrack/Demo.java
@@ -0,0 +1,14 @@
+package com.actitime.timetrack;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo {
+
+	@Test
+	public void runTest() {
+	Reporter.log("Successfully running",true);
+	
+	}
+}
