6e7dafb37bc077a59132900d4d957449211c8037
Raghav <Raghav@Raghav>
2020-08-07 17:01:14 +0530

    Demo2 Commit

---
+++ Actitime/src/test/java/com/actitime/timetrack/Demo2.java
@@ -0,0 +1,14 @@
+package com.actitime.timetrack;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Demo2 {
+
+	@Test
+	public void runTest() {
+	Reporter.log("Successfully running Demo2",true);
+	Reporter.log("Hi Hi I am Raghav Are able to see me in Jenkins???",true);
+	}
+}
