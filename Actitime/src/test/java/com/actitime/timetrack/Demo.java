059f1ec48881cc8cd92a4fd2d1aac6683764f8f5
Raghav <Raghav@Raghav>
2020-08-07 16:39:09 +0530

    Demo Jenkins Commit

--- Actitime/src/test/java/com/actitime/timetrack/Demo.java
+++ Actitime/src/test/java/com/actitime/timetrack/Demo.java
@@ -9,7 +9,7 @@ public class Demo {
 	@Test
 	public void runTest() {
 	Reporter.log("Successfully running",true);
-	
+	Reporter.log("Hi Hi I am Raghav Are able to see me in Jenkins???",true);
 	// Hi I am Raghav Are able to see me in Github???
 	}
 }
